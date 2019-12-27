25197dbf824b01a3843195a580dab375f88142ca
jonathan-martz <github@jmartz.de>
2019-12-27 19:03:32 +0100

    fix migrations name

--- migrations/2019_10_28_175139_add_role_user.php
+++ migrations/2019_10_28_175139_add_role_user.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AddRoleUserTable extends Migration
+class AddRoleUser extends Migration
 {
     /**
      * Run the migrations.
