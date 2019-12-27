80f1ba05f349a35af274596bd9f127edb965a675
jonathan-martz <github@jmartz.de>
2019-12-27 18:05:02 +0100

    initial commit

---
+++ migrations/2019_10_28_175139_add_role_user.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::exists('user_role')) {
+            $count = DB::table('user_role')->where('name', '=', 'user')->count();
+            if ($count === 0) {
+                DB::table('user_role')->insert([
+                    'name' => 'user'
+                ]);
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
---
+++ model/RoleUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class User
+ * @package App\Model
+ */
+class RoleUser extends Model
+{
+
+}
