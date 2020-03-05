9d9d116070bfc4f335a939aade55c8155dd2d61e
jonathan-martz <github@jmartz.de>
2020-03-05 10:07:40 +0100

    update requirements

--- model/RoleUser.php
+++ model/RoleUser.php
@@ -2,13 +2,11 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
-
 /**
  * Class User
  * @package App\Model
  */
-class RoleUser extends Model
+class RoleUser extends BaseRole
 {
 
 }
